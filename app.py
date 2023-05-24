ee789dcfab28d0245269f73bc19b61e63f9f3f09
fmallari <fc.mallari@gmail.com>
2023-05-23 23:21:07 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import stories
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def ask_story():
+    """show list-of-stories form"""
+
+    return render_template("select_story.html", stories=stories.values())
+
+@app.route("/questions")
+def ask_questions():
+    """Generate and show form to select words"""
+
+    story_id = request.args["story_id"]
+    story = stories[story_id]
+
+    prompts = story.prompts
+
+    return render_template("questions.html", story_id=story_id, title=story.title, prompts=prompts)
+
+    @app.route("/story")
+    def show_story():
+        """Results of story"""
+
+        story_id = request.args["story_id"]
+        story = story[story_id]
+
+        text = story.generate(request.args)
+
+        return render_template("story.html", title=story.title, text=text)
+
